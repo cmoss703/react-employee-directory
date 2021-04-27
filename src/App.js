5e413e2cab0f91f944c2b522d6b32d53d8d02055
Christina <christinalmoss@gmail.com>
2021-04-27 11:33:43 -0500

    folder restructuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Table from "./components/Table";
 
 function App() {
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
